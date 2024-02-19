5598e370e8ffcf540adca5e956c7ba8502fc7c80
isaac <idreyes@uh.edu>
2024-02-19 08:53:15 -0600

    ugh, updating to switch to windows -_-

--- modules/1-React/updated_vite/examples/part_a/src/App.tsx
+++ modules/1-React/updated_vite/examples/part_a/src/App.tsx
@@ -1,7 +1,18 @@
+const Hello = ({name, age}: {name: string, age: number}) => {
+    return(
+        <div>
+            <p> Hello {name}, you are {age} years old</p>
+        </div>
+    )
+}
 const App = () => {
+    const name = "Joe"
+    const age = 18
     return (
         <div>
-            <p> Hello world! </p>
+            <h1>Greetings</h1>
+            <Hello name='Isaac' age= {26 + 10}/>
+            <Hello name= {name} age= {age}/>
         </div>
     )
 }
